76754ca0b45069466051bd066da5d99aa5ed286e
DriesBos <info@driesbos.com>
2025-05-24 17:43:57 +0100

    cleanup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -65,24 +65,3 @@ export default function RootLayout({
     </html>
   );
 }
-
-// 'use client';
-
-// import { useThemeStore } from '@/providers/theme-store-provider';
-// import Footer from '@/components/footer/footer';
-// import Header from '@/components/header/header';
-// import Layout from '@/components/layout/layout';
-
-// export default function Home() {
-//   const theme = useThemeStore((state) => state.theme);
-
-//   return (
-//     <div className="page" data-theme={theme}>
-//       <Header />
-//       <main>
-//         <Layout />
-//       </main>
-//       <Footer />
-//     </div>
-//   );
-// }
