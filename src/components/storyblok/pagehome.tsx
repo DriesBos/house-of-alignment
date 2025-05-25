cebc5a114317123d1064cea1b0dc9be0ff4a6027
DriesBos <info@driesbos.com>
2025-05-25 13:54:03 +0100

    added gsap content sequential lrendering animation

--- src/components/storyblok/pagehome.tsx
+++ src/components/storyblok/pagehome.tsx
@@ -4,6 +4,7 @@ import {
   StoryblokServerComponent,
 } from '@storyblok/react/rsc';
 import React from 'react';
+import ContentColumn from '@/components/content-column';
 
 interface SbPagehomeData extends SbBlokData {
   columnone: SbBlokData[];
@@ -19,21 +20,21 @@ const Pagehome: React.FunctionComponent<PagehomeProps> = ({ blok }) => {
   console.log('Pagehome blok:', blok);
   return (
     <div className="page page-Home" {...storyblokEditable(blok)}>
-      <div className="page-Home_Column">
+      <ContentColumn className="page-Home_Column">
         {blok.columnone.map((nestedBlok) => (
           <StoryblokServerComponent blok={nestedBlok} key={nestedBlok._uid} />
         ))}
-      </div>
-      <div className="page-Home_Column">
+      </ContentColumn>
+      <ContentColumn className="page-Home_Column">
         {blok.columntwo.map((nestedBlok) => (
           <StoryblokServerComponent blok={nestedBlok} key={nestedBlok._uid} />
         ))}
-      </div>
-      <div className="page-Home_Column">
+      </ContentColumn>
+      <ContentColumn className="page-Home_Column">
         {blok.columnthree.map((nestedBlok) => (
           <StoryblokServerComponent blok={nestedBlok} key={nestedBlok._uid} />
         ))}
-      </div>
+      </ContentColumn>
     </div>
   );
 };
