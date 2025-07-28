72f519ff83b127847c7e5e70a890870fc18f2dfd
DriesBos <info@driesbos.com>
2025-07-28 16:12:32 +0100

    added: custom backgrounds

--- src/components/storyblok/content-blok/content-blok.tsx
+++ src/components/storyblok/content-blok/content-blok.tsx
@@ -6,6 +6,7 @@ import {
 import styles from './content-blok.module.sass';
 
 interface SbContentBlokData extends SbBlokData {
+  background?: string;
   body: SbBlokData[];
 }
 
@@ -14,10 +15,16 @@ interface ContentBlokProps {
 }
 
 export default function ContentBlok({ blok }: ContentBlokProps) {
-  console.log('ContentBlok', blok.body);
+  console.log('ContentBlok', blok);
 
   return (
-    <div className={styles.contentBlok} {...storyblokEditable(blok)}>
+    <div
+      className={styles.contentBlok}
+      {...storyblokEditable(blok)}
+      style={{
+        backgroundColor: blok.background || 'transparent',
+      }}
+    >
       {blok.body?.map((nestedBlok) => (
         <StoryblokServerComponent blok={nestedBlok} key={nestedBlok._uid} />
       ))}
