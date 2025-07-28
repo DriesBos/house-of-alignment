893b1c97097d71910f83bdad97365518b8f0054c
DriesBos <info@driesbos.com>
2025-07-28 09:23:25 +0100

    temp fix

--- src/components/storyblok/content-blok/content-blok.tsx
+++ src/components/storyblok/content-blok/content-blok.tsx
@@ -1,5 +1,5 @@
 import {
-  StoryblokServerComponent,
+  // StoryblokServerComponent,
   SbBlokData,
   storyblokEditable,
 } from '@storyblok/react/rsc';
@@ -19,7 +19,16 @@ export default function ContentBlok({ blok }: ContentBlokProps) {
   return (
     <div className={styles.contentBlok} {...storyblokEditable(blok)}>
       {blok.body?.map((nestedBlok) => (
-        <StoryblokServerComponent blok={nestedBlok} key={nestedBlok._uid} />
+        // <StoryblokServerComponent blok={nestedBlok} key={nestedBlok._uid} />
+        <p className="text" key={nestedBlok._uid}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
       ))}
     </div>
   );
