d99b2f016a3aede120d351f5c6a9d7e32ad31a82
DriesBos <info@driesbos.com>
2025-11-04 16:28:32 +0900

    cleanup header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -2,10 +2,8 @@
 
 import styles from './header.module.sass';
 import { useThemeStore } from '@/providers/theme-store-provider';
-import { useLayoutStore } from '@/providers/layout-store-provider';
 import { useCallback, useState, useEffect } from 'react';
 import type { ThemeState } from '@/stores/theme-store';
-import type { LayoutState } from '@/stores/layout-store';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { useGlobalData } from '@/providers/global-data-provider';
@@ -14,8 +12,6 @@ export default function Header() {
   const [isOpen, setIsOpen] = useState(false);
   const theme = useThemeStore((state) => state.theme);
   const setTheme = useThemeStore((state) => state.setTheme);
-  const layout = useLayoutStore((state) => state.layout);
-  const setLayout = useLayoutStore((state) => state.setLayout);
   const pathname = usePathname();
   const { globalData } = useGlobalData();
 
@@ -32,62 +28,47 @@ export default function Header() {
     setTheme(nextTheme);
   }, [setTheme, theme]);
 
-  const handleLayoutChange = useCallback(() => {
-    const layouts: LayoutState[] = ['one', 'two', 'three'];
-    const currentIndex = layouts.indexOf(layout);
-    const nextIndex = (currentIndex + 1) % layouts.length;
-    const nextLayout = layouts[nextIndex];
-    setLayout(nextLayout);
-  }, [setLayout, layout]);
-
   const toggleHeader = useCallback(() => {
     setIsOpen((prev) => !prev);
   }, []);
 
   return (
-    <>
-      {/* <div
-        className={styles.background}
-        onClick={toggleHeader}
-        data-active={isOpen}
-      /> */}
-      <header className={styles.header} data-active={isOpen}>
-        <div className={styles.header_top}>
-          <div className={styles.logo}>
-            <Link href="/">House of Alignment</Link>
-          </div>
-          <div
-            className={styles.menuIcon}
-            onClick={toggleHeader}
-            data-active={isOpen}
-          >
-            <div className={styles.menuIcon_bar} />
-            <div className={styles.menuIcon_bar} />
-          </div>
-          <div className={styles.slogan}>( {globalData.slogan} )</div>
+    <header className={styles.header} data-active={isOpen}>
+      <div className={styles.header_top}>
+        <div className={styles.logo}>
+          <Link href="/">House of Alignment</Link>
         </div>
-        <div className={styles.header_bottom}>
-          <nav className={styles.header_nav}>
-            <ul>
-              <li>
-                <Link href="/">Index</Link>
-              </li>
-              <li>
-                <Link href="/tags/dinners">Dinners</Link>
-              </li>
-              <li>
-                <Link href="/tags/interviews">Interviews</Link>
-              </li>
-              <li>
-                <Link href="/about">About</Link>
-              </li>
-            </ul>
-          </nav>
-          <div className={styles.header_buttons}>
-            <button onClick={handleThemeChange}>( Theme: {theme} )</button>
-          </div>
+        <div
+          className={styles.menuIcon}
+          onClick={toggleHeader}
+          data-active={isOpen}
+        >
+          <div className={styles.menuIcon_bar} />
+          <div className={styles.menuIcon_bar} />
         </div>
-      </header>
-    </>
+        <div className={styles.slogan}>( {globalData.slogan} )</div>
+      </div>
+      <div className={styles.header_bottom}>
+        <nav className={styles.header_nav}>
+          <ul>
+            <li>
+              <Link href="/">Index</Link>
+            </li>
+            <li>
+              <Link href="/tags/dinners">Dinners</Link>
+            </li>
+            <li>
+              <Link href="/tags/interviews">Interviews</Link>
+            </li>
+            <li>
+              <Link href="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
+        <div className={styles.header_buttons}>
+          <button onClick={handleThemeChange}>( Theme: {theme} )</button>
+        </div>
+      </div>
+    </header>
   );
 }
