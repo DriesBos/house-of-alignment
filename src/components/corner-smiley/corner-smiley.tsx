05a80541158cf44f73ec9ee2980adccd76971947
DriesBos <info@driesbos.com>
2026-01-24 19:21:51 +0900

    Style: Limit smiley object to routes

--- src/components/corner-smiley/corner-smiley.tsx
+++ src/components/corner-smiley/corner-smiley.tsx
@@ -1,7 +1,19 @@
+'use client';
+
+import { usePathname } from 'next/navigation';
 import ThreeDContainer from '../three-d-container/three-d-container';
 import styles from './corner-smiley.module.sass';
 
 const CornerSmiley = () => {
+  const pathname = usePathname();
+  const isHome = pathname === '/';
+  const isTagsLanding = pathname === '/tags';
+  const isTagsRoute = pathname.startsWith('/tags/');
+
+  if (!isHome && !isTagsLanding && !isTagsRoute) {
+    return null;
+  }
+
   return (
     <div className={`${styles.cornerSmiley} cornerSmiley cursorInteract`}>
       <ThreeDContainer />
